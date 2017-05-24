6808c449351b90bbe4b2e245f6a0d2c2ec78468a
TheJosecraft <josekawa1996@gmail.com>
2017-05-24 21:57:26 +0200

    Modalidades 2 Jugadores
    Se han añadido las modalidades normal y aleatorio del modo 2 jugadores

--- practica.sh
+++ practica.sh
@@ -625,25 +625,32 @@ break;;
 	"Ránking")
 	#echo "NOMBRE" "PUNTUACIÓN" "MODALIDAD" "CATEGORÍA"
 	#echo "$(cat $FICHPUNT)" | sort -k2 -nr 
+	echo ""
 	printf "%-25s %-25s %-24s %-25s\n" "NOMBRE" "PUNTUACIÓN" "MODALIDAD" "CATEGORÍA"
 	printf "%-25s %-24s %-25s %-25s\n" $(cat $FICHPUNT) | sort -k2 -nr
+	echo ""
+	
+	echo "Selecciona una opción"
+	printf "%-17s %-17s %-15s\n" "1) Normal" "3) Aleatorio" "5) Ránking"
+	printf "%-18s %-17s %-15s\n" "2) Categorías" "4) Dos Jugadores" "6) Salir"
 	break;;
 	"Dos Jugadores")
-		echo "Plata o plomo"
+		read -p "Introduce el nombre del Jugador 1: " J1
+		read -p "Introduce el nombre del jugador 2: " J2
 		echo "Selecciona una opción"
-		select OPCIONES in "Normal" "Categorías" "Aleatorio"
+		
+		select OPCIONES in "Normal" "Categorías" "Aleatorio" "Salir"
 		do
-			while [ "$OPCIONES" != "3" ]
+			while [ "$OPCIONES" != "4" ]
 			do
 				case $OPCIONES in
 
 				"Normal")
+	PUNTUACIONTJ1=0
+	PUNTUACIONTJ2=0
 	PUNTUACIONT=0
 	MODALIDAD="NORMAL"
 	CATEG="TODO"
-	
-	read -p "Introduce el nombre del Jugador 1: " J1
-	read -p "Introduce el nombre del jugador 2: " J2
 	
 	for(( i = 1; i <= $NUMERO; i = i + 1))
 	do
@@ -680,9 +687,9 @@ break;;
 			fi
 
 		done
-
-		if [ "$RESPUESTA" == "$PALABRA"]; then
-			if [ $[ $CONTADOR % 2 ] -eq 0 ]; then
+		MODULO=$[ PUNTUACION % 2 ]
+		if [ "$RESPUESTA" == "$PALABRA" ]; then
+			if [ $MODULO -eq 0 ]; then
 				echo "Correcto, has acertado. El jugador $J1 ha conseguido $PUNTUACION puntos"
 			else
 				echo "Correcto, has acertado. El jugador $J2 ha conseguido $PUNTUACION puntos"
@@ -706,40 +713,137 @@ break;;
 		done
 
 		if [ "$CONFIRMAR" == "Y" ]; then
-			PUNTUACIONT=$[ PUNTUACION + PUNTUACIONT ]
+			if [ $MODULO -eq 0 ]; then
+				PUNTUACIONTJ1=$[ PUNTUACION + PUNTUACIONTJ1 ]
+			else
+				PUNTUACIONTJ2=$[ PUNTUACION + PUNTUACIONTJ2 ]
+			fi
 			echo "Buena suerte"
 			echo ""
 		elif [ "$CONFIRMAR" == "N" ]; then
-			PUNTUACIONT=$[ PUNTUACION + PUNTUACIONT ]
-			read -p "Introduce tu nombre para guardar la puntuación: " NOMBRE
-			echo $NOMBRE "$PUNTUACIONT $MODALIDAD $CATEG" >> $FICHPUNT
-			echo "Puntuación total: $PUNTUACIONT"
+			PUNTUACIONTJ1=$[ PUNTUACION + PUNTUACIONTJ1 ]
+			PUNTUACIONTJ2=$[ PUNTUACION + PUNTUACIONTJ2 ]
+			if [ $MODULO -eq 0 ]; then
+				echo $J1 "$PUNTUACIONTJ1 $MODALIDAD $CATEG" >> $FICHPUNT
+			else
+				echo $J2 "$PUNTUACIONTJ2 $MODALIDAD $CATEG" >> $FICHPUNT
+			fi
+			
+			echo "Puntuación total de $J1: $PUNTUACIONTJ1"
+			echo "Puntuación total de $J2: $PUNTUACIONTJ2"
 			i=$[ NUMERO + 1 ]
 		else
 			echo "ERROR, INTRODUCE UN VALOR CORRECTO"
 		fi
-	echo ""
-	echo "Selecciona una opción"
-
-	printf "%-17s %-17s %-15s\n" "1) Normal" "3) Aleatorio" "5) Ránking"
-	printf "%-18s %-17s %-15s\n" "2) Categorías" "4) Dos Jugadores" "6) Salir"
+	
 	done
 				break;;
 
 				"Categorías")
-
+					echo "Podrás jugar en este modo en actualizaciones futuras. Perdona las molestias"
 				break;;
 
 				"Aleatorio")
+	PUNTUACIONTJ1=0
+	PUNTUACIONTJ2=0
+	PUNTUACIONT=0
+	CONFIRMAR="Y"
+	MODALIDAD="ALEATORIO"
+	CATEG="TODO"
+	while [ "$CONFIRMAR" == "Y" ]
+	do
+		ALEATORIO=$[RANDOM % $NUMERO +1]
+		LINEA=$(head -n 1 adivinanza$ALEATORIO.txt)
+		echo $LINEA
 
+		CONTADOR=0
+		PUNTUACION=10
+		RESPUESTA=$(head -n 2 adivinanza$ALEATORIO.txt | tail -n 1)
+		echo ""
+
+		PALABRA=""
+		echo ""
+		while [ "$RESPUESTA" != "$PALABRA" ]  && [ $CONTADOR -lt 10 ]
+		do
+
+			CONTADOR=$[ $CONTADOR + 1 ] 
+
+
+			PISTA=$(tail -n 10 adivinanza$ALEATORIO.txt | head -n $CONTADOR | tail -n 1)
+			echo $PISTA
+
+			read -t 20 -p "Introduce la respuesta: " PAL
+
+			PALABRA=$PAL
+			echo ""
+
+			if [ "$RESPUESTA" == "$PALABRA" ]; then
+				CONTADOR=10
+			else
+				PUNTUACION=$[ PUNTUACION - 1 ]
+			fi
+
+		done
+
+		MODULO=$[ PUNTUACION % 2 ]
+		if [ "$RESPUESTA" == "$PALABRA" ]; then
+			if [ $MODULO -eq 0 ]; then
+				echo "Correcto, has acertado. El jugador $J1 ha conseguido $PUNTUACION puntos"
+			else
+				echo "Correcto, has acertado. El jugador $J2 ha conseguido $PUNTUACION puntos"
+			fi
+			
+		else
+			echo "La próxima vez será"
+			echo -n "La respuesta correcta es : $RESPUESTA" 
+		fi
+
+		read -p "¿Quieres seguir jugando? Y/N " CONFIRMAR
+
+		while [ "$CONFIRMAR" != "Y" ] && [ "$CONFIRMAR" != "N" ] 
+		do
+			read -p "¿Quieres seguir jugando? Y/N " CONFIRMAR
+		done
+
+		if [ "$CONFIRMAR" == "Y" ]; then
+			if [ $MODULO -eq 0 ]; then
+				PUNTUACIONTJ1=$[ PUNTUACION + PUNTUACIONTJ1 ]
+			else
+				PUNTUACIONTJ2=$[ PUNTUACION + PUNTUACIONTJ2 ]
+			fi
+			echo "Buena suerte"
+			echo ""
+		elif [ "$CONFIRMAR" == "N" ]; then
+			PUNTUACIONTJ1=$[ PUNTUACION + PUNTUACIONTJ1 ]
+			PUNTUACIONTJ2=$[ PUNTUACION + PUNTUACIONTJ2 ]
+			if [ $MODULO -eq 0 ]; then
+				echo $J1 "$PUNTUACIONTJ1 $MODALIDAD $CATEG" >> $FICHPUNT
+			else
+				echo $J2 "$PUNTUACIONTJ2 $MODALIDAD $CATEG" >> $FICHPUNT
+			fi
+			
+			echo "Puntuación total de $J1: $PUNTUACIONTJ1"
+			echo "Puntuación total de $J2: $PUNTUACIONTJ2"
+			i=$[ NUMERO + 1 ]
+		else
+			echo "ERROR, INTRODUCE UN VALOR CORRECTO"
+		fi
+	done
 				break;;
-
+				"Salir")
+				exit
+					break;;
 				*)
 					echo "Opción no válida"
 					break;;
 			esac
 			done
 		done
+		echo ""
+	echo "Selecciona una opción"
+
+	printf "%-17s %-17s %-15s\n" "1) Normal" "3) Aleatorio" "5) Ránking"
+	printf "%-18s %-17s %-15s\n" "2) Categorías" "4) Dos Jugadores" "6) Salir"
 		break;;
 	"Salir")
 	exit
